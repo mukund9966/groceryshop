2bcf16c2ee914c12260c29c36513b51789b56939
Saransh Kumar <cryptosingh@gmail.com>
2020-09-24 23:56:46 +0530

    Initial Commit

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include"datamodel/Stock.h"
+using namespace std;
+
+void printWelcome() {
+    cout<<"************Welcome to the Budget Food - The Grocery Store************\n";
+    cout<<
+}
+
+int main() {
+
+    return 0;
+}
